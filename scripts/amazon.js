8805c8c4e9ed892061d1139334b951a2a743820e
KaynatTanaj <kaynattanaj@gmail.com>
2024-08-19 18:48:48 +0600

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,6 +81,17 @@ document.querySelector('.js-products-grid')
                     quantity: 1
                 });
             }
-            console.log(cart);
+
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
+
+           // console.log(cartQuantity);
+           // console.log(cart);
         });
     });
