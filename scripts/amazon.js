23417fab5ce0bb94cd23007fb3ffd19ebeff7ec9
KaynatTanaj <kaynattanaj@gmail.com>
2024-09-06 19:29:45 +0600

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,126 +1,130 @@
 import { cart, addToCart, calculateCartQuantity } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
-products.forEach((product) => {
-    productsHTML += `
-        <div class="product-container">
-            <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
-            </div>
+loadProducts(renderProductsGrid);
 
-            <div class="product-name limit-text-to-2-lines">
-                ${product.name}
-            </div>
-
-            <div class="product-rating-container">
-                <img class="product-rating-stars"
-                  src="${product.getStarsUrl()}">
-                <div class="product-rating-count link-primary">
-                    ${product.rating.count}
+function renderProductsGrid() {
+    let productsHTML = '';
+    products.forEach((product) => {
+        productsHTML += `
+            <div class="product-container">
+                <div class="product-image-container">
+                    <img class="product-image"
+                    src="${product.image}">
                 </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                    ${product.name}
+                </div>
+
+                <div class="product-rating-container">
+                    <img class="product-rating-stars"
+                    src="${product.getStarsUrl()}">
+                    <div class="product-rating-count link-primary">
+                        ${product.rating.count}
+                    </div>
+                </div>
+
+                <div class="product-price">
+                    ${product.getPrice()}
+                </div>
+
+                <div class="product-quantity-container">
+                    <select class="js-quantity-selector-${product.id}">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    </select>
+                </div>
+
+                ${product.extraInfoHTML()}
+
+                <div class="product-spacer"></div>
+
+                <div class="added-to-cart js-added-to-cart-${product.id}">
+                    <img src="images/icons/checkmark.png">
+                    Added
+                </div>
+
+
+
+                <button class="add-to-cart-button button-primary js-add-to-cart"
+                data-product-id="${product.id}">
+                    Add to Cart
+                </button>
             </div>
-
-            <div class="product-price">
-                ${product.getPrice()}
-            </div>
-
-            <div class="product-quantity-container">
-                <select class="js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                </select>
-            </div>
-
-            ${product.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-                <img src="images/icons/checkmark.png">
-                Added
-            </div>
-
-
-
-            <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-id="${product.id}">
-                Add to Cart
-            </button>
-        </div>
-    `;
-});
-
-document.querySelector('.js-products-grid')
- .innerHTML = productsHTML;
-
-function updateCartQuantity() {
-    const cartQuantity = calculateCartQuantity();
-  
-    document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
-        // console.log(cartQuantity);
-  }    
-
-/*this function is being called here again because 
-in real time the cart should get updated too,not only when
-clicking the add-to-cart button*/
-updateCartQuantity();
-
-
- document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-
-        let addedMessageTimeoutId;
-
-        button.addEventListener('click', () => {
-            /*used the destructuring method to combine the variable
-              and the other value together
-              const productId = button.dataset.productId;*/
-
-            const {productId} = button.dataset ;
-            /* ".dataset" finds out all the data attributes
-             attached to the html element */
-            
-            const quantitySelector = document.querySelector(
-                `.js-quantity-selector-${productId}`);  
-            const quantity = Number(quantitySelector.value);
-
-            addToCart(productId, quantity);   
-
-            const addedMessage = document.querySelector(
-                `.js-added-to-cart-${productId}`);
-
-            addedMessage.classList.add('added-to-cart-visible');
-
-            setTimeout(() => {
-                // Check if a previous timeoutId exists. If it does,
-                // we will stop it.
-                if (addedMessageTimeoutId) {
-                  clearTimeout(addedMessageTimeoutId);
-                }
-          
-                const timeoutId = setTimeout(() => {
-                  addedMessage.classList.remove('added-to-cart-visible');
-                }, 2000);
-          
-                // Save the timeoutId so we can stop it later.
-                addedMessageTimeoutId = timeoutId;
-
-            });
-
-            updateCartQuantity()
-
-           // console.log(cart);
-        });
+        `;
     });
+
+    document.querySelector('.js-products-grid')
+    .innerHTML = productsHTML;
+
+    function updateCartQuantity() {
+        const cartQuantity = calculateCartQuantity();
+    
+        document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity;
+            // console.log(cartQuantity);
+    }    
+
+    /*this function is being called here again because 
+    in real time the cart should get updated too,not only when
+    clicking the add-to-cart button*/
+    updateCartQuantity();
+
+
+    document.querySelectorAll('.js-add-to-cart')
+        .forEach((button) => {
+
+            let addedMessageTimeoutId;
+
+            button.addEventListener('click', () => {
+                /*used the destructuring method to combine the variable
+                and the other value together
+                const productId = button.dataset.productId;*/
+
+                const {productId} = button.dataset ;
+                /* ".dataset" finds out all the data attributes
+                attached to the html element */
+                
+                const quantitySelector = document.querySelector(
+                    `.js-quantity-selector-${productId}`);  
+                const quantity = Number(quantitySelector.value);
+
+                addToCart(productId, quantity);   
+
+                const addedMessage = document.querySelector(
+                    `.js-added-to-cart-${productId}`);
+
+                addedMessage.classList.add('added-to-cart-visible');
+
+                setTimeout(() => {
+                    // Check if a previous timeoutId exists. If it does,
+                    // we will stop it.
+                    if (addedMessageTimeoutId) {
+                    clearTimeout(addedMessageTimeoutId);
+                    }
+            
+                    const timeoutId = setTimeout(() => {
+                    addedMessage.classList.remove('added-to-cart-visible');
+                    }, 2000);
+            
+                    // Save the timeoutId so we can stop it later.
+                    addedMessageTimeoutId = timeoutId;
+
+                });
+
+                updateCartQuantity()
+
+            // console.log(cart);
+            });
+        });
+}
